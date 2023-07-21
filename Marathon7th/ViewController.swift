3d661546838fb822dd849ed5c12e0861181c3622
Ilia Kosov <kosov11@gmail.com>
2023-07-21 13:19:14 +0300

    Initial Commit

---
+++ Marathon7th/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Marathon7th
+//
+//  Created by Ilya Kosov on 21.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
